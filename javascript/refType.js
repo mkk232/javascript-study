9dd6a3832426736f0c8aaff280b816c635747467
mkk232 <mkk232@naver.com>
2023-01-17 10:42:37 +0900

    레퍼런스 타입과 동일성 테스트

--- javascript/refType.js
+++ javascript/refType.js
@@ -1,22 +1,22 @@
 // 참조타입과 동일성 테스트
 
-'hi' === 'hi' // true
+console.log('hi' === 'hi') // true
 
-['hi', 'bye'] == ['hi', 'bye'] // false
+console.log(['hi', 'bye'] == ['hi', 'bye']) // false
 
 console.log([1] === [1]) // false
 
-console.log([1] == [1]) // false
+console.log([1] === [1]) // false
 
-console.log([] == []) // false
+console.log([] === []) // false
 
 let luckyNum = 87; // 메모리 어딘가에 저장될 것이다.
 
 let luckyNums = [2, 235, 62]; // 여기서 저장되는건 참조다. 
 
-[1, 2, 3] // 이 배열의 고유한 주소를 저장한다.
+// [1, 2, 3] 이 배열의 고유한 주소를 저장한다.
 
-[1, 2, 3] === [1, 2, 3] // 서로 주소가 다르기 때문에 false가 나옴.
+console.log([1, 2, 3] === [1, 2, 3]) // 서로 주소가 다르기 때문에 false가 나옴.
 
 let nums = [1, 2, 3];
 
@@ -41,4 +41,4 @@ console.log(nums) // [1, 2, 3]
 
 console.log(numsCopy) // [1, 2, 3]
 
-nums === numsCopy // true
+console.log(nums === numsCopy) // true
