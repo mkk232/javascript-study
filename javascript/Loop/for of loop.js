e284dbc486fe43c721e3606d6b8edba804ed97b6
mkk232 <mkk232@naver.com>
2023-01-17 14:57:29 +0900

    for of 반복문

---
+++ javascript/Loop/for of loop.js
@@ -0,0 +1,35 @@
+// for .... of
+// Internet Explorer 지원 x
+// for loop에서 개선됨.
+
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'basketball', 'tennis'];
+
+for (let i = 0; i < subreddits.length; i++) {
+    console.log(`Visit reddit.com/r/${subreddits[i]}`);
+}
+
+// 더욱 깔끔하게 !?
+
+// Java의 상향된 for문과 비슷한듯..
+
+// for .... of
+for (let sub of subreddits) {
+    console.log(`Visit reddit.com/r/${sub}`);
+}
+
+// seatingChart 요소 모두 출력하기.
+const seatingChart = [
+    ['Kristen', 'Erik', 'Namita'],
+    ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+    ['Yuma', 'Sakura', 'Jack', 'Erika']
+]
+
+for (let row of seatingChart) {
+    for (let student of row) {
+        console.log(student);
+    }
+}
+
+for (let char of "hello world") {
+    console.log(char);
+}
