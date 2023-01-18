5eb9361752e1a3b5cf297236b3669e38de67ae40
mkk232 <mkk232@naver.com>
2023-01-18 16:58:16 +0900

    함수 표현식

---
+++ javascript/Function/scope/funtionExpression.js
@@ -0,0 +1,16 @@
+/*
+    함수 표현식
+*/
+// function add(x, y) {
+//     return x + y;
+// }
+
+// add(3, 4);
+
+const add = function (x, y) {
+    return x + y;
+}
+
+console.log(add(1, 3));
+
+// 함수도 값으로 표현될 수 있다.
